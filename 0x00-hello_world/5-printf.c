5909ee5195180ffd7292842a12ea6f61d5d7aef1
Zynabomar <zumar4334@gmail.com>
2022-09-09 00:33:38 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - print the string in the put fuction 
+*
+* Description: using the main fuction 
+* this program prints "programming is like building a multingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
